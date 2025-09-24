18d2115bcd6e4cf259bd071e43312b6111cb111f
Maxime Tavares <t.maxime59@gmail.com>
2025-09-24 12:07:01 +0200

    Anwser mocking example

--- src/test/java/com/myproject/debug/Service/BatchCalculatorServiceTest.java
+++ src/test/java/com/myproject/debug/Service/BatchCalculatorServiceTest.java
@@ -2,8 +2,10 @@ package com.myproject.debug.Service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -39,27 +41,62 @@ public class BatchCalculatorServiceTest {
     public void givenOperationsList_whenbatchCalculate_thenCallsServiceWithCorrectArguments()
             throws IOException, URISyntaxException {
         // GIVEN
-        final Stream<String> operations =
-            Arrays.asList("2 + 2", "5 - 4", "6 x 8", "9 / 3").stream();
-        final ArgumentCaptor<CalculationModel> calculationModelCaptor =
-            ArgumentCaptor.forClass(CalculationModel.class);
+        final Stream<String> operations = Arrays.asList("2 + 2", "5 - 4", "6 x 8", "9 / 3").stream();
+        final ArgumentCaptor<CalculationModel> calculationModelCaptor = ArgumentCaptor.forClass(CalculationModel.class);
 
         // WHEN
         batchCalculatorService.batchCalculate(operations);
 
         // THEN
         verify(calculatorService, times(4)).calculate(calculationModelCaptor.capture());
-        final List<CalculationModel> calculationModels =
-            calculationModelCaptor.getAllValues();
+        final List<CalculationModel> calculationModels = calculationModelCaptor.getAllValues();
         assertThat(calculationModels)
-                .extracting(CalculationModel::getLeftArgument, 
-                            CalculationModel::getType,
-                            CalculationModel::getRightArgument)
+                .extracting(CalculationModel::getLeftArgument,
+                        CalculationModel::getType,
+                        CalculationModel::getRightArgument)
                 .containsExactly(
                         tuple(2, CalculationType.ADDITION, 2),
                         tuple(5, CalculationType.SUBTRACTION, 4),
                         tuple(6, CalculationType.MULTIPLICATION, 8),
                         tuple(9, CalculationType.DIVISION, 3));
+    }
+    
+    // Anwser mocking example
+    @Test
+    public void givenOperationsList_whenbatchCalculate_thenCallsServiceAndReturnsAnswer()
+            throws IOException, URISyntaxException {
+        // GIVEN
+        final Stream<String> operations =
+            Arrays.asList("2 + 2", "5 - 4", "6 x 8", "9 / 3").stream();
+        when(calculatorService.calculate(any(CalculationModel.class)))
+        .then(invocation -> {
+            final CalculationModel model = invocation.getArgument(0, CalculationModel.class);
+            switch (model.getType()) {
+            case ADDITION:
+                model.setSolution(4);
+                break;
+            case SUBTRACTION:
+                model.setSolution(1);
+                break;
+            case MULTIPLICATION:
+                model.setSolution(48);
+                break;
+            case DIVISION:
+                model.setSolution(3);
+                break;
+            default:
+            }
+            return model;
+        });
+
+        // WHEN
+        final List<CalculationModel> results =
+            batchCalculatorService.batchCalculate(operations);
+
+        // THEN
+        verify(calculatorService, times(4)).calculate(any(CalculationModel.class));
+        assertThat(results).extracting("solution").containsExactly(4, 1, 48, 3);
+
     }
 
 }
